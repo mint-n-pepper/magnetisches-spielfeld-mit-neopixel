89a2dd184e001be7b39676a31509f4315327d90d
mint-n-pepper <122439150+mint-n-pepper@users.noreply.github.com>
2023-08-14 15:25:57 +0200

    separate sending function

--- main.ts
+++ main.ts
@@ -29,17 +29,15 @@ namespace MagnetischeNavigation {
      * @param leistung die der Elektromagnet abgeben soll
      */
     //% block="Setze Leistung für Elektromagnet $index auf $leistung"
-    //% leistung.min=-255 leistung.max=255
+    //% leistung.min=-100 leistung.max=100
     //% index.min=1 index.max=8
     //% leistung.defl=0
     //% index.defl=1
-    export function setMotorSpeed(
+    export function setMagnetPower(
         index?: number,
         leistung?: number) {
         let motorDriverIdx = Math.floor((index - 1) / 2)
         let motorDriverPort = (index - 1) % 2
-        let directionBuffer = pins.createBuffer(3)
-        let speedBuffer = pins.createBuffer(3)
 
         // set new direction
         if (leistung < 0) {
@@ -49,56 +47,67 @@ namespace MagnetischeNavigation {
         }
         // set new speed
         electromagnetOutput[motorDriverIdx][motorDriverPort] = Math.abs(leistung)
-        if (electromagnetOutput[motorDriverIdx][motorDriverPort] > 255) {
-            electromagnetOutput[motorDriverIdx][motorDriverPort] = 255
+        if (electromagnetOutput[motorDriverIdx][motorDriverPort] > 100) {
+            electromagnetOutput[motorDriverIdx][motorDriverPort] = 100
         }
+    }
 
-        //set direction buffer
-        directionBuffer[0] = DirectionSet
-        if (electromagnetDirection[motorDriverIdx][0] == 0 && electromagnetDirection[motorDriverIdx][1] == 0) {
-            directionBuffer[1] = BothAntiClockWise
-        } else if (electromagnetDirection[motorDriverIdx][0] == 0 && electromagnetDirection[motorDriverIdx][1] == 1) {
-            directionBuffer[1] = M1ACWM2CW
-        } else if (electromagnetDirection[motorDriverIdx][0] == 1 && electromagnetDirection[motorDriverIdx][1] == 0) {
-            directionBuffer[1] = M1CWM2ACW
-        } else {
-            //both are forward (1)
-            directionBuffer[1] = BothClockWise
-        }
-        directionBuffer[2] = Nothing
-        pins.i2cWriteBuffer(DriverAddress[motorDriverIdx], directionBuffer, false)
 
-        //set power
-        speedBuffer[0] = MotorSpeedSet
-        speedBuffer[1] = electromagnetOutput[motorDriverIdx][0]
-        speedBuffer[2] = electromagnetOutput[motorDriverIdx][1]
-        pins.i2cWriteBuffer(DriverAddress[motorDriverIdx], speedBuffer, false)
-        
-//set led strips
+    /**
+     * Schicke alle Leistungen zu den Motortreibern und aktiviere die Neopixel
+     */
+    //% block="Sende alle Leistungswerte"
+    export function writeAll() {
+        let directionBuffer = pins.createBuffer(3)
+        let speedBuffer = pins.createBuffer(3)
+               
+        //set led strips
         strip.clear();
         let motorIdx=0;
         let ledStartIdx=0;
         for (let driverIdx = 0; driverIdx < 4; driverIdx++) {
-    	    for (let portIdx = 0; portIdx < 2; portIdx++) {
+            //set direction buffer
+            directionBuffer[0] = DirectionSet
+            if (electromagnetDirection[driverIdx][0] == 0 && electromagnetDirection[driverIdx][1] == 0) {
+                directionBuffer[1] = BothAntiClockWise
+            } else if (electromagnetDirection[driverIdx][0] == 0 && electromagnetDirection[driverIdx][1] == 1) {
+                directionBuffer[1] = M1ACWM2CW
+            } else if (electromagnetDirection[driverIdx][0] == 1 && electromagnetDirection[driverIdx][1] == 0) {
+                directionBuffer[1] = M1CWM2ACW
+            } else {
+                //both are forward (1)
+                directionBuffer[1] = BothClockWise
+            }
+            directionBuffer[2] = Nothing
+            pins.i2cWriteBuffer(DriverAddress[driverIdx], directionBuffer, false)
+
+            //set power
+            speedBuffer[0] = MotorSpeedSet
+            speedBuffer[1] = Math.floor(electromagnetOutput[driverIdx][0]*2.55)
+            speedBuffer[2] = Math.floor(electromagnetOutput[driverIdx][1]*2.55)
+            pins.i2cWriteBuffer(DriverAddress[driverIdx], speedBuffer, false)
+
+            //set all LED lights
+            for (let portIdx = 0; portIdx < 2; portIdx++) {
                 motorIdx=driverIdx*2+portIdx
                 ledStartIdx=motorIdx*8
                 let colorChoice = neopixel.rgb(0, 255, 0)
                 if (electromagnetDirection[driverIdx][portIdx]>0) {
                     colorChoice = neopixel.rgb(255, 0, 0)
                       }
-                if (electromagnetOutput[driverIdx][portIdx] > 35) {
+                if (electromagnetOutput[driverIdx][portIdx] > 10) {
                     strip.setPixelColor(ledStartIdx+3, colorChoice)
                     strip.setPixelColor(ledStartIdx+4, colorChoice)
                 }
-                if (electromagnetOutput[driverIdx][portIdx] > 110) {
+                if (electromagnetOutput[driverIdx][portIdx] > 40) {
                     strip.setPixelColor(ledStartIdx+2, colorChoice)
                     strip.setPixelColor(ledStartIdx+5, colorChoice)
                 }
-                if (electromagnetOutput[driverIdx][portIdx] > 160) {
+                if (electromagnetOutput[driverIdx][portIdx] > 70) {
                     strip.setPixelColor(ledStartIdx+1, colorChoice)
                     strip.setPixelColor(ledStartIdx+6, colorChoice)
                 }
-                if (electromagnetOutput[driverIdx][portIdx] > 220) {
+                if (electromagnetOutput[driverIdx][portIdx] > 95) {
                     strip.setPixelColor(ledStartIdx, colorChoice)
                     strip.setPixelColor(ledStartIdx+7, colorChoice)
                 }
@@ -109,4 +118,12 @@ namespace MagnetischeNavigation {
 
     }
 
+    /**
+     * Setze Leistung für alle Elektromagnete auf 0
+     */
+    //% block="Setze Leistung für alle Elektromagnete auf 0"
+    export function zeroAllMagnets() {
+        electromagnetDirection = [[0, 0], [0, 0], [0, 0], [0, 0]]
+        electromagnetOutput = [[0, 0], [0, 0], [0, 0], [0, 0]]    
+    }
 }
